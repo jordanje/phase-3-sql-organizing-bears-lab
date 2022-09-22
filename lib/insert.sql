a4c4c51d22ac53b309cc0c2fa154399a78591f8e
Jordan Jennings <jordanjennings12@gmail.com>
2022-09-22 11:57:10 -0400

    commit tree a778d2e6e5fb7e8e9c7748e10111aa4b4767e940

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "M", "Brown", "Passive", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 4, "M", "Black", "mean", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 8, "F", "Brown", "Passive", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 2, "M", "Brown", "Passive", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 10, "F", "Brown", "Aggressive", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 6, "M", "Brown", "Aggressive", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 2, "F", "Brown", "Passive", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 2, "M", "Brown", "Passive", true);
